76ba170b4caa1141f89f91fd509514d1332d0caf
Paulo Elias <paulo@ideo.com>
2019-10-21 12:23:10 -0700

    Add site meta

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,26 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Tailwind CSS + Emotion Starter`,
-    description: `A bare-bones Tailwind CSS + Emotion starter to kickoff your project. `,
-    author: `@pauloelias`,
+    title: `Paulo Elias - Technical leadership, product ownership, and product engineering.`,
+    description: `Paulo Elias is a technical lead, senior engineer, and very subpar designer. He loves to talk shop about modern web + mobile devcelopment, teaching folks new tools and techniques, and enjoys public speaking.`,
+    author: {
+      name: `Paulo Elias`,
+      byLine: `Per libero elit sollicitudin taciti faucibus metus natoque sapien viverra et, lorem est phasellus nam nisl litora erat interdum auctor, aptent ultricies pulvinar mi dis varius nascetur dictum fusce.`,
+    },
+    organization: {
+      name: "Paulo Elias",
+      url: "https://lengstorf.com",
+      logo: "https://lengstorf.com/android-chrome-512x512.png",
+    },
+    siteUrl: "https://www.pauloelias.com",
+    image: "",
+    social: {
+      twitter: "@pauloelias",
+      instagram: "@pauloelias",
+      github: "@pauloelias",
+      fbAppID: "",
+    },
   },
+  pathPrefix: "/",
   plugins: [
     `gatsby-plugin-emotion`,
     `gatsby-plugin-postcss`,
