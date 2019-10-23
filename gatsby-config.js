032e22a0de4f68fbcb917410a65e1c421b722c6e
Paulo Elias <paulo@ideo.com>
2019-10-23 16:30:42 -0700

    Remove mdx rendering for gatsby pages

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,13 +26,6 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        name: `pages`,
-        path: `${__dirname}/src/pages/`,
-      },
-    },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
         name: `posts`,
         path: `${__dirname}/src/content/posts/`,
       },
