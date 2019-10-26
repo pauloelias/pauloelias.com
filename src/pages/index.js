45475e6cad264a35dd23cd101bf48cadf00ab8f3
Paulo Elias <paulo@ideo.com>
2019-10-26 09:24:21 -0700

    Add scrathc page for fiddling around procrastinating

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,68 @@
 import tw from "tailwind.macro"
+import { css } from "@emotion/core"
 import React from "react"
+import { Link, graphql } from "gatsby"
+
 import SEO from "../components/seo"
 
-const Heading = tw.h1`
-  text-2xl
+const Container = tw.div`
+  mx-auto
 `
 
-export default () => (
-  <>
+const Heading = tw.h1`
+  font-sans font-semibold text-2xl
+`
+
+const Excerpt = tw.h3`
+  font-serif text-1xl
+`
+
+export default ({ data }) => (
+  <Container className="container">
     <SEO title="Hi, I'm Paulo Elias!" />
     <Heading>Hello, world!</Heading>
-  </>
+    <h4>{data.allMdx.totalCount} Posts</h4>
+    {data.allMdx.edges.map(({ node }) => (
+      <div key={node.id}>
+        <Link
+          to={`/post/${node.frontmatter.slug}`}
+          css={css`
+            text-decoration: none;
+            color: inherit;
+          `}
+        >
+          <Excerpt>
+            {node.frontmatter.title}{" "}
+            <span
+              css={css`
+                color: #bbb;
+              `}
+            >
+              — {node.frontmatter.date}
+            </span>
+          </Excerpt>
+          <p>{node.frontmatter.description}</p>
+        </Link>
+      </div>
+    ))}
+  </Container>
 )
+
+export const pageQuery = graphql`
+  query {
+    allMdx(sort: { fields: [frontmatter___date], order: DESC }) {
+      totalCount
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            date(formatString: "DD MMMM, YYYY")
+            slug
+            description
+          }
+        }
+      }
+    }
+  }
+`
