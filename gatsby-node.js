7899766551973fe43063da61540997412e2e89ba
Paulo Elias <paulo@ideo.com>
2019-10-26 09:23:54 -0700

    Pull journal content for now

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,3 +1,51 @@
+const path = require("path")
+const { createFilePath } = require(`gatsby-source-filesystem`)
+
+exports.onCreateNode = ({ node, getNode, actions }) => {
+  const { createNodeField } = actions
+  if (node.internal.type === `Mdx`) {
+    const slug = createFilePath({ node, getNode, basePath: `journal` })
+    createNodeField({
+      node,
+      name: `slug`,
+      value: slug,
+    })
+  }
+}
+
+exports.createPages = async ({ graphql, actions, reporter }) => {
+  const { createPage } = actions
+
+  const result = await graphql(`
+    query {
+      allMdx(filter: { fileAbsolutePath: { regex: "/journal/" } }) {
+        edges {
+          node {
+            id
+            frontmatter {
+              slug
+            }
+          }
+        }
+      }
+    }
+  `)
+
+  if (result.errors) {
+    reporter.panicOnBuild('🚨  ERROR: Loading "createPages" query')
+  }
+
+  const posts = result.data.allMdx.edges
+
+  posts.forEach(({ node }, index) => {
+    createPage({
+      path: `/blog/${node.frontmatter.slug}`,
+      component: path.resolve(`./src/components/posts-page.js`),
+      context: { id: node.id },
+    })
+  })
+}
+
 exports.onCreateWebpackConfig = ({ actions, getConfig }) => {
   // Hack due to Tailwind ^1.1.0 using `reduce-css-calc` which assumes node
   // https://github.com/bradlc/babel-plugin-tailwind-components/issues/39#issuecomment-526892633
