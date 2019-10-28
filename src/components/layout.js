58469586c9461e2b5d3ccc2a47e06e8583cc1b1c
Paulo Elias <paulo@ideo.com>
2019-10-28 08:34:42 -0700

    Add base layout and remove bgColor

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,19 +1,16 @@
 import tw from "tailwind.macro"
+import { css } from "@emotion/core"
 import React from "react"
 
 import Header from "./header"
 import Footer from "./footer"
 
-const Container = tw.div`
-  mx-auto max-w-full
-`
-
 const Layout = ({ children }) => (
-  <Container>
+  <div css={css`${tw`mx-auto max-w-full`}`}>
     <Header />
     <main>{children}</main>
     <Footer />
-  </Container>
+  </div>
 )
 
 export default Layout
