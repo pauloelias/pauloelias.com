ef616cf4cf1108bdc593c6e82e177a3cae9cc836
Paulo Elias <paulo@ideo.com>
2019-10-28 14:50:14 -0700

    Fix listing pages

--- gatsby-node.js
+++ gatsby-node.js
@@ -7,11 +7,11 @@ function generateSlug(folder, path) {
   return `/${folder}/${slugify(path)}`
 }
 
-function createJournalPages(createPage, edges) {
+const createEntryPages = (createPage, edges) => {
   edges.forEach(({ node }) => {
     createPage({
       path: node.fields.slug,
-      component: path.resolve(`./src/templates/journal-detail.js`),
+      component: path.resolve(`./src/templates/detail.js`),
       context: {
         id: node.id,
       },
@@ -24,7 +24,9 @@ function createPaginatedPages(
   entries,
   paginationTemplate,
   pathPrefix,
-  filter
+  filter,
+  pageHeading,
+  pageDescription
 ) {
   const numPages = Math.ceil(entries.length / POST_PER_PAGE)
 
@@ -39,6 +41,8 @@ function createPaginatedPages(
         currentPage: i + 1,
         filter,
         pathPrefix,
+        pageHeading,
+        pageDescription,
       },
     })
   })
@@ -51,6 +55,9 @@ exports.createPages = async ({ graphql, actions, reporter }) => {
     `
       fragment BaseMdxFields on Mdx {
         id
+        fields {
+          slug
+        }
         frontmatter {
           title
         }
@@ -64,9 +71,6 @@ exports.createPages = async ({ graphql, actions, reporter }) => {
           edges {
             node {
               ...BaseMdxFields
-              fields {
-                slug
-              }
             }
           }
         }
@@ -103,9 +107,15 @@ exports.createPages = async ({ graphql, actions, reporter }) => {
 
   // Grab all journal entries
   const journalEntries = result.data.journalEntries.edges
+  const speakingEntries = result.data.speakingEntries.edges
+  const interviewEntries = result.data.interviewEntries.edges
 
   // Create Journal Entries
-  createJournalPages(createPage, journalEntries)
+  createEntryPages(createPage, journalEntries)
+  // Create Speaking Entries
+  createEntryPages(createPage, speakingEntries)
+  // Create Interview Entries
+  createEntryPages(createPage, interviewEntries)
 
   // Create paginted journal entries
   createPaginatedPages(
@@ -113,25 +123,31 @@ exports.createPages = async ({ graphql, actions, reporter }) => {
     journalEntries,
     `./src/templates/listing.js`,
     `journal`,
-    `/journal/`
+    `/journal/`,
+    `Journal`,
+    `Ut ipsum euismod ridiculus varius aliquam lorem, neque scelerisque mattis vulputate pulvinar dapibus, litora fringilla justo lectus interdum.`
   )
 
   // Create Speakinf Entries List
   createPaginatedPages(
     createPage,
-    result.data.speakingEntries.edges,
+    speakingEntries,
     `./src/templates/listing.js`,
     `speaking`,
-    `/speaking/`
+    `/speaking/`,
+    `Public Speaking`,
+    `Ut ipsum euismod ridiculus varius aliquam lorem, neque scelerisque mattis vulputate pulvinar dapibus, litora fringilla justo lectus interdum.`
   )
 
   // Create Interviews Entries List
   createPaginatedPages(
     createPage,
-    result.data.interviewEntries.edges,
+    interviewEntries,
     `./src/templates/listing.js`,
     `interviews`,
-    `/interviews/`
+    `/interviews/`,
+    `Interviews`,
+    `Ut ipsum euismod ridiculus varius aliquam lorem, neque scelerisque mattis vulputate pulvinar dapibus, litora fringilla justo lectus interdum.`
   )
 }
 
@@ -141,11 +157,23 @@ exports.onCreateNode = ({ node, actions, getNode }) => {
   if (node.internal.type === `Mdx`) {
     const parent = getNode(node.parent)
     let slug =
-      slugify(node.frontmatter.title) || createFilePath({ node, getNode })
+      slugify(node.frontmatter.title) ||
+      createFilePath({ node, getNode, basePath: `pages` })
 
     if (node.fileAbsolutePath.includes("content/journal/")) {
       slug =
         generateSlug("journal", node.frontmatter.title) || slugify(parent.name)
+    }
+
+    if (node.fileAbsolutePath.includes("content/speaking/")) {
+      slug =
+        generateSlug("speaking", node.frontmatter.title) || slugify(parent.name)
+    }
+
+    if (node.fileAbsolutePath.includes("content/interviews/")) {
+      slug =
+        generateSlug("interviews", node.frontmatter.title) ||
+        slugify(parent.name)
     }
 
     createNodeField({
--- src/templates/listing.js
+++ src/templates/listing.js
@@ -3,14 +3,17 @@ import { css } from "@emotion/core"
 import React from "react"
 import { Link, graphql } from "gatsby"
 
-const Excerpt = tw.h3`
-  font-serif text-1xl
-`
-
 export default function SpeakingListing({ data, pageContext }) {
   console.log(data, pageContext)
   const entries = data.allMdx.edges
-  const { currentPage, numPages, pathPrefix } = pageContext
+  // TODO: Move pagination into its own component
+  const {
+    currentPage,
+    numPages,
+    pathPrefix,
+    pageHeading,
+    pageDescription,
+  } = pageContext
   const isFirst = currentPage === 1
   const isLast = currentPage === numPages
   const prevPage =
@@ -18,57 +21,101 @@ export default function SpeakingListing({ data, pageContext }) {
       ? `/${pathPrefix}`
       : `/${pathPrefix}/${(currentPage - 1).toString()}`
   const nextPage = `/${pathPrefix}/${(currentPage + 1).toString()}`
+
   return (
-    <div>
-      {entries.map(({ node }) => (
-        <div key={node.id}>
-          <Link
-            to={node.fields.slug}
+    <>
+      {pageHeading && (
+        <h2
+          css={css`
+            ${tw`text-blue-900 font-sans font-medium text-xl sm:font-normal sm:text-2xl md:text-3xl lg:text-4xl`}
+          `}
+        >
+          {pageHeading}
+        </h2>
+      )}
+
+      {pageDescription && (
+        <p
+          css={css`
+            ${tw`mt-3 lg:mt-6 text-lg sm:text-xl lg:text-2xl text-gray-700`}
+          `}
+        >
+          {pageDescription}
+        </p>
+      )}
+
+      <div
+        css={css`
+          ${tw`mt-6 text-lg text-gray-700 lg:mt-8 lg:mt-10 sm:text-xl lg:text-2xl`}
+        `}
+      >
+        {entries.map(({ node }) => (
+          <div
+            key={node.id}
             css={css`
-              text-decoration: none;
-              color: inherit;
+              ${tw`border-t border-gray-400 py-6 sm:py-8 lg:py-10`}
             `}
           >
-            <Excerpt>
-              {node.frontmatter.title}{" "}
-              <span
+            <Link
+              to={node.fields.slug}
+              css={css`
+                text-decoration: none;
+                color: inherit;
+              `}
+            >
+              <h3
                 css={css`
-                  color: #bbb;
+                  ${tw`text-gray-900 font-serif text-lg lg:text-xl`}
                 `}
               >
-                — {node.frontmatter.date}
-              </span>
-            </Excerpt>
-            <p>{node.frontmatter.description}</p>
-          </Link>
-        </div>
-      ))}
-      {!isFirst && (
-        <Link to={prevPage} rel="prev">
-          ← Previous Page
-        </Link>
-      )}
-      {!isFirst &&
-        !isLast &&
-        Array.from({ length: numPages }, (_, i) => (
-          <Link
-            key={`pagination-number${i + 1}`}
-            to={`/speaking/${i === 0 ? "" : i + 1}`}
-          >
-            {i + 1}
-          </Link>
+                <span
+                  css={css`
+                    ${tw`mb-1 block text-gray-500 font-sans text-sm font-semibold uppercase md:text-base lg:text-lg md:mb-2`}
+                  `}
+                >
+                  {node.frontmatter.date}
+                </span>
+                {node.frontmatter.title}
+              </h3>
+              <p
+                css={css`
+                  ${tw`mt-2 text-gray-700 text-sm sm:text-base lg:text-lg md:mt-3`}
+                `}
+              >
+                {node.frontmatter.description}
+              </p>
+            </Link>
+          </div>
         ))}
-      {!isLast && (
-        <Link to={nextPage} rel="next">
-          Next Page →
-        </Link>
-      )}
-    </div>
+        <div>
+          {!isFirst && (
+            <Link to={prevPage} rel="prev">
+              ← Previous Page
+            </Link>
+          )}
+          {!isFirst &&
+            !isLast &&
+            Array.from({ length: numPages }, (_, i) => (
+              <Link
+                key={`pagination-number${i + 1}`}
+                to={`/speaking/${i === 0 ? "" : i + 1}`}
+              >
+                {i + 1}
+              </Link>
+            ))}
+          {!isLast && (
+            <Link to={nextPage} rel="next">
+              Next Page →
+            </Link>
+          )}
+        </div>
+      </div>
+    </>
   )
 }
 
 export const pageQuery = graphql`
-  query speakingListQuery($skip: Int!, $limit: Int!, $filter: String!) {
+  query entriesListQuery($skip: Int!, $limit: Int!, $filter: String!) {
     allMdx(
       filter: { fileAbsolutePath: { regex: $filter } }
       sort: { fields: [frontmatter___date], order: DESC }
@@ -85,6 +132,8 @@ export const pageQuery = graphql`
             date(formatString: "DD MMMM, YYYY")
             title
             description
+            url
+            resourcesUrl
           }
         }
       }
