afa1d2fe4590ee28e0d68e5d8a960d14cfcc3a50
Paulo Elias <paulo@ideo.com>
2019-10-29 11:18:07 -0700

    Add sie config

---
+++ config/site.js
@@ -0,0 +1,32 @@
+module.exports = {
+  siteTitle: "Paulo Elias", // Navigation and Site Title
+  siteTitleAlt: "The personal website of Paulo Elias", // Alternative Site title for SEO
+  siteTitleShort: "pauloelias", // short_name for manifest
+  siteUrl: process.env.ROOT_URL || "https://www.pauloelias.com.com", // Domain of your site. No trailing slash!
+  siteLanguage: "en", // Language Tag on <html> element
+  siteLogo: "images/logo.png", // Used for SEO and manifest, path to your image you placed in the 'static' folder
+  siteDescription:
+    "Paulo Elias is a technical lead, frontend engineer, and a very subpar designer. He loves to talk shop about modern web, teaching folks new tools and techniques, and tinkering with new technology.",
+  minibio: "TBD",
+  author: "Paulo Elias", // Author for schemaORGJSONLD
+  organization: "QuickPixel",
+
+  // siteFBAppID: "", // Facebook App ID - Optional
+  userTwitter: "@pauloelias", // Twitter Username
+  ogSiteName: "Paulo Elias", // Facebook Site Name
+  ogLanguage: "en_US",
+  googleAnalyticsID: "",
+
+  // Manifest and Progress color
+  themeColor: "",
+  backgroundColor: "",
+
+  // Social component
+  github: "https://github.com/pauloelias/",
+  instagram: "https://twitter.com/pauloelias/",
+  instagramHandle: "@pauloelias",
+  twitter: "https://twitter.com/pauloelias/",
+  twitterHandle: "@pauloelias",
+  linkedin: "https://www.linkedin.com/in/pauloelias/",
+  rss: "https://www.pauloelias.com/journal/rss.xml",
+}
