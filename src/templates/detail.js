025a1d96aa15c06cdee7169cb4b2da7ff938c800
Paulo Elias <paulo@ideo.com>
2019-10-29 20:43:27 -0700

    Move mdx styles out of emotion globals to tailwind global css

--- src/templates/detail.js
+++ src/templates/detail.js
@@ -1,5 +1,5 @@
 import tw from "tailwind.macro"
-import { Global, css } from "@emotion/core"
+import { css } from "@emotion/core"
 import React from "react"
 import { graphql } from "gatsby"
 import { MDXRenderer } from "gatsby-plugin-mdx"
@@ -9,17 +9,6 @@ import { Heading } from "../components/ui/text"
 export default function JoutnalDetail({ data: { mdx } }) {
   return (
     <div className="detail-page">
-      <Global
-        styles={css`
-          .gatsby-highlight,
-          .gatsby-resp-image-wrapper {
-            ${tw`mt-4 mb-4 lg:mt-10 lg:mb-10`}
-          }
-          .gatsby-highlight {
-            ${tw`lg:mx-auto lg:w-4/5`}
-          }
-        `}
-      />
       <Heading level="h2">{mdx.frontmatter.title}</Heading>
       <div
         css={css`
